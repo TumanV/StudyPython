3bce42676781003cd2f8780f9b1ea46b75edc721
User <tumanviktor21@gmail.com>
2019-04-22 23:06:48 +0300

    Hi, hi

--- tasks/1_variables.py
+++
@@ -1,5 +0,0 @@
-# Создать переменные строкового, числового и булевого типов данных
-num_1 = input ("Enter first num: ")
-num_2 = input ("Enter second num: ")
-res = num_1 + num_2
-print("Resalt is", res)
